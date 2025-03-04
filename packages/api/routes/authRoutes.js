a861a70f5181eacc98eaad282fa830942cfc40ec
Juan Roa <hello@juanroa.dev>
2025-03-04 13:18:20 -0600

    added new layer of validations for basic service protection, error handling

--- packages/api/routes/authRoutes.js
+++ packages/api/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { register, login } = require('../controllers/AuthController');
+const { validateRegistration, validateLogin } = require('../middleware/validation');
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegistration, register);
+router.post('/login', validateLogin, login);
+
 
 module.exports = router;
