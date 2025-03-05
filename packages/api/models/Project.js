d75b85549a4804cc264dfe333f7bd01c1462d5a3
Juan Roa <hello@juanroa.dev>
2025-03-04 19:56:51 -0600

    upgraded role permissions to single users

--- packages/api/models/Project.js
+++ packages/api/models/Project.js
@@ -1,35 +1,52 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-const User = require('./User');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const User = require("./User");
+const Team = require("./Team");
 
-const Project = sequelize.define('Project', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id',
+const Project = sequelize.define(
+  "Project",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    onDelete: 'CASCADE',
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: User,
+        key: "id",
+      },
+      onDelete: "SET NULL",
+    },
+    teamId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: Team,
+        key: "id",
+      },
+      onDelete: "SET NULL",
+    },
   },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-User.hasMany(Project, { foreignKey: 'userId', onDelete: 'CASCADE' });
-Project.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Project, { foreignKey: "ownerId", onDelete: "SET NULL" });
+Project.belongsTo(User, { foreignKey: "ownerId" });
+
+Team.hasMany(Project, { foreignKey: "teamId", onDelete: "SET NULL" });
+Project.belongsTo(Team, { foreignKey: "teamId" });
 
 module.exports = Project;
